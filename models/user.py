71d42958b6898cf54e4e99b3dd5f55657a2de66a
Innocente Niwemwana <i.niwemwana@alustudent.com>
2024-09-28 22:11:51 +0530

    1 file

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = 'i.niwemwana@alustudent.com'
+    password = 'jojo2003'
+    first_name = 'Innocente'
+    last_name = 'Aline'
