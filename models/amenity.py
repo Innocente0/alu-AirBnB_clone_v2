0cf41110a6cbe30235dd3a14a289784975e38a42
Innocente Niwemwana <i.niwemwana@alustudent.com>
2024-10-01 17:24:56 +0530

    all files

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python3
-"""
-- Provides the Amenity class,represents an amenity for accommodations.
-"""
-from models.base_model import BaseModel
+#!/usr/bin/python3
+""" State Module for HBNB project """
+import os
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """
-    - Represents an amenity for accommodations.
-    """
-
-    def __init__(self, *args, **kwargs):
-
-        """
-        Initializes a new instance of the Amenity class.
-        """
-
-        super().__init__(*args, **kwargs)
-        self.name = ""
+class Amenity(BaseModel, Base):
+    """Amenities of a place"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary="place_amenity")
